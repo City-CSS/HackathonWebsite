6e83ddaa348c783680f818f2a7c0fbaf1f5c2212
Supermarcel10 <marcelbarlik@gmail.com>
2024-01-24 21:22:57 +0000

    - Created template using Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		colors: {
+			'text': '#15090e',
+			'background': '#fefbfc',
+			'primary': '#d77fa4',
+			'secondary': '#5d223a',
+			'accent': '#a92d61',
+		},
+
+		fontSize: {
+			sm: '0.750rem',
+			base: '1rem',
+			xl: '1.333rem',
+			'2xl': '1.777rem',
+			'3xl': '2.369rem',
+			'4xl': '3.158rem',
+			'5xl': '4.210rem'
+		},
+		fontFamily: {
+			heading: 'Noto Sans Runic',
+			body: 'Noto Sans Runic'
+		},
+		fontWeight: {
+			normal: '400',
+			bold: '700'
+		}
+	},
+	plugins: []
+}
