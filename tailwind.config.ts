c0276e424a1d6a598fa4ec0e4dd2ecb4ca3e2d2c
Supermarcel10 <marcelbarlik@gmail.com>
2024-02-03 22:21:52 +0000

    - Added required tailwindcss/forms for Contact.tsx - Updated icons to use Bootstrap Icons and React Bootstrap Icons due to easy implementations and large quantity of icons to choose from

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -106,7 +106,7 @@ const config = {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")]
 } satisfies Config
 
 export default config
