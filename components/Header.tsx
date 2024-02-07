ad3de6848dd23c40c8af91832f517ad35980f0a5
marcel <marcelbarlik@gmail.com>
2024-02-07 22:42:03 +0000

    - Replace button with the tailwind react CSS button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
-import {Button} from "@/components/ui/button";
+import {Button} from "@material-tailwind/react";
+import React from "react";
+
 
 export default function Header() {
-
-	return(
-
+	return (
 		<header className="text-gray-600 body-font">
 			<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 				<nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
@@ -16,8 +16,8 @@ export default function Header() {
 					<span className="ml-3 text-xl">City Hackathon</span>
 				</a>
 				<div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-					<Button  className="inline-flex items-center  bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-						<a className="text-gray-600" href="/profile">Get Tickets!</a>
+					<Button placeholder="" className="inline-flex items-center  bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+						<a className="text-gray-600" href="/tickets">Get Tickets!</a>
 					</Button>
 				</div>
 			</div>
