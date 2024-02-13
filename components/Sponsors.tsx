53b0a632533c7a6884d3336c696497d54da7e209
Supermarcel10 <marcelbarlik@gmail.com>
2024-02-13 14:52:26 +0000

    - Connected Sponsors to database. - Added a backup load error image. - Added a temporary solution to contact for sponsorship enquiry.

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,17 +1,23 @@
+import {supabase} from "@/app/api/supabaseClient"
 import CenteringGrid from "@/components/CenteringGrid"
+import TEMP_SponsorButton from "@/components/TEMP_SponsorButton"
 import {Button} from "@material-tailwind/react"
+import {Error} from "@/components/InfoMessages"
 import Image from "next/image"
 import Link from "next/link"
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 
 export default function Sponsors() {
+	const [error, setError] = React.useState<string>("");
+
 	enum SponsorType {
 		PLATINUM = 'Platinum',
 		GOLD = 'Gold',
 		SILVER = 'Silver',
 		BRONZE = 'Bronze',
-		SUPPORTER = 'Supporter'
+		SUPPORTER = 'Supporter',
+		PLACEHOLDER = 'Placeholder'
 	}
 
 	interface Sponsor {
@@ -21,42 +27,75 @@ export default function Sponsors() {
 		type: SponsorType;
 	}
 
-	const sponsors : Sponsor[] = [
-		// {
-		// 	name : "Transistor",
-		// 	link : "https://transistor.com",
-		// 	image : "https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg",
-		// 	type : SponsorType.PLATINUM
-		// }
-	]
+	const [sponsors, setSponsors] = useState<Sponsor[]>([]);
+
+	useEffect(() => {
+		const fetchSponsors = async () => {
+			try {
+				let { data: sponsors, error } = await supabase
+					.from('Sponsors')
+					.select('name, url, tier, logo_uri');
+
+				console.log(error)
+				if (error) {
+					setError("Unable to load sponsors! Please try again later.")
+				}
+
+				const transformedSponsors = sponsors
+					? sponsors.map((sponsor) => ({
+						name: sponsor.name,
+						link: sponsor.url,
+						image: sponsor.logo_uri || "",
+						type: SponsorType[sponsor.tier as keyof typeof SponsorType]
+					}))
+					: [{ name: "", link: "", image: "", type: SponsorType.PLACEHOLDER }];
+
+				setSponsors(transformedSponsors);
+			} catch (error) {}
+		};
+
+		fetchSponsors();
+	}, []);
 
 	return (
 		<div className="mx-auto bg-secondary-50 text-text-950 py-24 sm:py-32 px-4 lg:px-12 max-w-full">
 			<h2 className={`mx-auto text-3xl sm:text-4xl font-bold tracking-tight text-center px-12 max-w-2xl ${sponsors.length > 0? "pb-12" : ""}`}>Our Sponsors</h2>
-			<CenteringGrid colSize={3} iterator={sponsors}>
-				{(sponsor, index, widthClass) => (
-					<Link href={sponsor.link} key={index} className={widthClass + " px-2 py-2 flex justify-center items-center"} draggable="false">
-						<Image
-							src={sponsor.image}
-							alt={sponsor.name + " logo"}
-							width={158}
-							height={48}
-						/>
-					</Link>
-				)}
-			</CenteringGrid>
 
-			{sponsors.length > 0 ? (
-				<p className="text-center text-lg pt-12 pb-4">Want to sponsor? 😀</p>
+			{error !== "" ? (
+				<Error message={error}/>
 			) : (
-					<p className="text-center text-lg pb-4">No sponsors yet. 😔<br/>Do you want to be the first?</p>
+				<>
+					<CenteringGrid colSize={3} iterator={sponsors}>
+						{(sponsor, index, widthClass) => (
+							<Link href={sponsor.link} key={index} className={widthClass + " px-2 py-2 flex justify-center items-center"} draggable="false">
+								<Image
+									src={sponsor.image}
+									onError={(e) => {
+										(e.target as HTMLImageElement).src = '/ImageLoadError.svg';
+									}}
+									alt={sponsor.name + " logo"}
+									width={158}
+									height={48}
+								/>
+							</Link>
+						)}
+					</CenteringGrid>
+
+					{sponsors.length > 0 ? (
+						<p className="text-center text-lg pt-12 pb-4">Want to sponsor? 😀</p>
+					) : (
+						<p className="text-center text-lg pb-4">No sponsors yet. 😔<br/>Do you want to be the first?</p>
+					)}
+				</>
 			)}
 
 			<div className="flex flex-wrap justify-center gap-4">
-				{/*TODO: Implement sponsor page*/}
-				<Link href="/sponsor" draggable="false">
-					<Button size="lg" className="bg-accent-500 hover:bg-accent-600 text-text-950 focus:outline-red-700 text-lg py-2 px-4">Sponsor Citython</Button>
-				</Link>
+				<TEMP_SponsorButton/>
+
+				{/*/!*TODO: Implement sponsor page*!/*/}
+				{/*<Link href="/sponsor" draggable="false">*/}
+				{/*	<Button size="lg" className="bg-accent-500 hover:bg-accent-600 text-text-950 focus:outline-red-700 text-lg py-2 px-4">Sponsor Citython</Button>*/}
+				{/*</Link>*/}
 			</div>
 		</div>
 	)
---
+++ components/TEMP_SponsorButton.tsx
@@ -0,0 +1,104 @@
+import {supabase} from "@/app/api/supabaseClient"
+import {Error, Success} from "@/components/InfoMessages"
+import Link from "next/link"
+import React from "react"
+import {
+	Button,
+	Checkbox,
+	Dialog,
+	DialogBody,
+	DialogFooter,
+	DialogHeader,
+	Input,
+	Textarea,
+	Typography
+} from "@material-tailwind/react"
+
+
+export default function TEMP_SponsorButton() {
+	const [open, setOpen] = React.useState<boolean>(false);
+	const handleOpen = () => setOpen(!open);
+
+	const [error, setError] = React.useState<string>("");
+	const [success, setSuccess] = React.useState<boolean>(false);
+
+	const handleSend = async () => {
+		const emailInput = (document.getElementById("contactEmailInput") as HTMLInputElement);
+		const contentInput = (document.getElementById("contentInput") as HTMLTextAreaElement);
+		const privacyPolicyAgreedInput = (document.getElementById("privacyPolicyAgreedInput") as HTMLInputElement);
+
+		const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
+		const genericRegex = /[`'"<>;=]/
+
+		const isEmailValid = emailInput.value.trim() !== "" && emailRegex.test(emailInput.value.trim())
+		const isContentValid = contentInput.value.trim() !== "" && !genericRegex.test(contentInput.value.trim())
+		const isPrivacyPolicyAgreed = privacyPolicyAgreedInput.checked
+
+		emailInput.classList.toggle("outline-1", !isEmailValid)
+		emailInput.classList.toggle("outline-red-500", !isEmailValid)
+		emailInput.classList.toggle("bg-red-50", !isEmailValid)
+
+		contentInput.classList.toggle("outline-1", !isContentValid)
+		contentInput.classList.toggle("outline-red-500", !isContentValid)
+		contentInput.classList.toggle("bg-red-50", !isContentValid)
+
+		privacyPolicyAgreedInput.classList.toggle("outline-1", !isPrivacyPolicyAgreed)
+		privacyPolicyAgreedInput.classList.toggle("outline-red-500", !isPrivacyPolicyAgreed)
+		privacyPolicyAgreedInput.classList.toggle("bg-red-50", !isPrivacyPolicyAgreed)
+
+		if (isEmailValid && isContentValid && isPrivacyPolicyAgreed) {
+			try {
+				const {error} = await supabase
+					.from("Questions")
+					.insert({
+						email : emailInput.value, question : "SE:\n" + contentInput.value
+					});
+
+				if (error) {
+					setError("An error occurred. Please try again later.")
+				}
+
+				setError("")
+				setSuccess(true)
+
+				setTimeout(handleOpen, 3000)
+			} catch (error) {
+				setError("An error occurred. Please try again later.")
+			}
+		}
+	}
+
+	return (
+		<div>
+			<div className="flex justify-center items-center pt-8 pb-12">
+				<Button onClick={handleOpen} size="lg" className=" bg-accent-500 hover:bg-accent-600 text-text-950 focus:outline-red-700 text-lg py-2 px-4">
+					Sponsor Citython
+				</Button>
+			</div>
+
+			<Dialog open={open} handler={handleOpen}>
+				<DialogHeader>Sponsorship Enquiry</DialogHeader>
+				<DialogBody>
+					<div className="flex flex-col gap-4">
+						<Input id="contactEmailInput" variant="outlined" label="Email" placeholder="markzuckenberg@facebook.com" required/>
+						<Textarea id="contentInput" variant="outlined" label="Content" required/>
+						<Checkbox
+							id="privacyPolicyAgreedInput"
+							label={<Typography className="flex font-medium text-text-950">
+								I agree to the&nbsp;
+								<Link href="/policy.pdf" className="transition-colors text-accent-700 hover:text-accent-800 focus:outline-red-700" tabIndex={4}>privacy policy</Link>.
+							</Typography>}
+							required
+						/>
+					</div>
+					{error && <Error className="mt-2" message={error}/>}
+					{success && <Success className="mt-2" message="Enquiry submitted successfully! We'll get back to you soon."/>}
+				</DialogBody>
+				<DialogFooter className="flex justify-between">
+					<Button size="sm" onClick={handleOpen} className="bg-red-300 hover:bg-red-600 text-gray-900 focus:outline-red-700 text-lg py-2 px-4" tabIndex={2}>Cancel</Button>
+					<Button size="lg" onClick={handleSend} className="bg-accent-500 hover:bg-accent-600 text-accent-950 focus:outline-red-700 disabled:bg-accent-200 disabled:text-accent-800 text-lg py-2 px-4" tabIndex={1}>Send</Button>
+				</DialogFooter>
+			</Dialog>
+		</div>
+	)
+}
