e350050d902b797b904c5bf79b9826a9e3efbcf3
Supermarcel10 <marcelbarlik@gmail.com>
2024-02-16 01:49:27 +0000

    - Fixed scaling issues across all viewports

--- components/TitlePage.tsx
+++ components/TitlePage.tsx
@@ -9,23 +9,23 @@ const TitlePage = () => {
 	return (
 		<div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
 			<div className="mx-auto max-w-3xl text-center">
-				<div className="grid grid-cols-2 gap-6 mb-32 text-xl font-bold">
+				<div className="grid sm:grid-cols-2 gap-6 mb-32 lg:text-xl font-bold">
 					<div className="inline-flex items-center justify-center">
-						<CalendarFill className="mr-3 text-accent-500 text-2xl"/>
+						<CalendarFill className="mr-2 text-accent-500 text-lg sm:text-2xl"/>
 						<span className="font-bold">March 2nd & 3rd</span>
 					</div>
 					<Link href="https://www.google.com/maps/dir//City,+University+of+London,+Northampton+Square,+London+EC1V+0HB/@51.5279719,-0.1050373,17z/data=!4m17!1m7!3m6!1s0x48761ca7b1d83351:0x570d19c20ab22a83!2sCity,+University+of+London!8m2!3d51.5279719!4d-0.1024624!16zL20vMDJtZzVy!4m8!1m0!1m5!1m1!1s0x48761ca7b1d83351:0x570d19c20ab22a83!2m2!1d-0.1024624!2d51.5279719!3e3?entry=ttu"
 					      className="inline-flex items-center justify-center">
-						<GeoAltFill className="mr-2 text-accent-500 text-2xl"/>
+						<GeoAltFill className="mr-2 text-accent-500 text-lg sm:text-2xl"/>
 						<span className="font-bold">City, University of London</span>
 					</Link>
 				</div>
 
-				<h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-7xl lg:text-9xl">
+				<h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text  font-extrabold text-transparent text-7xl lg:text-9xl">
 					Citython<br/>2024
 				</h1>
 
-				<h2 className="mx-auto text-text-50 mt-4 max-w-xl sm:text-xl">
+				<h2 className="mx-auto text-text-50 mt-1 sm:mt-4 max-w-xl sm:text-xl">
 					City University&apos;s annual hackathon is back!
 				</h2>
 
