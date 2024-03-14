bdb0965ca149e9375b349386b24bcc7ddb29c612
marcel <marcelbarlik@gmail.com>
2024-03-14 13:15:34 +0000

    - Removed unused imports in unsubscribe page

--- app/unsubscribe/page.tsx
+++ app/unsubscribe/page.tsx
@@ -1,20 +1,6 @@
 "use client"
 
 import React from "react";
-import AboutUs from "@/components/home/AboutUs"
-// import Team from "@/components/Team"
-import Footer from "@/components/home/Footer";
-import Hero from "@/components/home/Hero";
-import WhyAttend from "@/components/home/WhyAttend";
-import Sponsors from "@/components/home/Sponsors";
-// import Timetable from "@/components/Timetable";
-import FAQ from "@/components/home/FAQ";
-import Timetable from "@/components/Timetable";
-import Team from "@/components/Team";
-import Stats from "@/components/home/old/Stats";
-import CenteringGrid from "@/components/CenteringGrid";
-import {Info} from "@/components/InfoMessages";
-import Question from "@/components/Question";
 import Waitlist from "@/components/Waitlist";
 
 
