d53ea9b889ed0c18ef2197ab7a43d2e2b1708d96
Filippo Vicini <filippo.vicini2@gmail.com>
2024-03-15 14:35:25 +0000

    bug fixes

--- app/unsubscribe/page.tsx
+++ app/unsubscribe/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import React from "react";
-import Waitlist from "@/components/Waitlist";
+import Unsubscribe from "@/components/Unsubscribe";
 
 
 export default function Home() {
 	return (
 		<div className="min-h-screen flex flex-col bg-background-950 text-text-50 select-none">
 			<main className="flex-grow pattern">
-				<Waitlist/>
+				<Unsubscribe/>
 			</main>
 		</div>
 	)
